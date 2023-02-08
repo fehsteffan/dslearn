f5bfb6d8504d15dedc66768c320f038fec4d0f05
Felipe Steffan <felip.developer@hotmail.com>
2023-02-07 21:05:16 -0300

    Course

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -1,88 +1,79 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="tb_user")
-public class User implements Serializable {
+@Table(name="tb_course")
+public class Course implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	private String email;
-	private String password;
+	private String imgUri;
+	private String imgGrayUri;
 	
 	
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name= "tb_user_role", joinColumns =  @JoinColumn(name="user_id"),
-	inverseJoinColumns = @JoinColumn(name="role_id"))
-	private Set<Role> roles = new HashSet<>();
-	
-	public User() {
+	public Course() {
 		
 	}
 
-	public User(Long id, String name, String email, String password) {		
+
+	public Course(Long id, String name, String imgUri, String imgGrayUri) {
+		super();
 		this.id = id;
 		this.name = name;
-		this.email = email;
-		this.password = password;
+		this.imgUri = imgUri;
+		this.imgGrayUri = imgGrayUri;
 	}
+
 
 	public Long getId() {
 		return id;
 	}
 
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 
 	public String getName() {
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getEmail() {
-		return email;
+
+	public String getImgUri() {
+		return imgUri;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+
+	public void setImgUri(String imgUri) {
+		this.imgUri = imgUri;
 	}
 
-	public String getPassword() {
-		return password;
+
+	public String getImgGrayUri() {
+		return imgGrayUri;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+
+	public void setImgGrayUri(String imgGrayUri) {
+		this.imgGrayUri = imgGrayUri;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}	
-
-	public Set<Role> getRoles() {
-		return roles;
-	}
 
 	@Override
 	public int hashCode() {
@@ -92,6 +83,7 @@ public class User implements Serializable {
 		return result;
 	}
 
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -100,13 +92,13 @@ public class User implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		Course other = (Course) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
+	}	
 	
 }
